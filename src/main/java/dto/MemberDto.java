6bdcfa994b166db588be7761fe40113505583ebc
hanseongseo <seeman94@naver.com>
2022-09-15 15:54:13 +0900

    - MemberDto.memberId 수정    member_id -> memberId
    - MemberDto import 정리

--- src/main/java/dto/MemberDto.java
+++ src/main/java/dto/MemberDto.java
@@ -5,21 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
-import java.util.Objects;
-
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter @Setter
 public class MemberDto {
 
     private Integer id;
-    private String member_id;
+    private String memberId;
     private String password;
     private String nickname;
     private String phone;
 
     public MemberDto(String member_id, String password, String nickname, String phone) {
-        this.member_id = member_id;
+        this.memberId = member_id;
         this.password = password;
         this.nickname = nickname;
         this.phone = phone;
