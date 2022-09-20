a04c41eacbc7ef0028f9049a32bf6fb61ed14c1f
hanseongseo <seeman94@naver.com>
2022-09-20 18:33:59 +0900

    GroupDetailHandler.java -> 그룹 참여 메소드 구현

--- src/main/java/handler/group/GroupDetailHandler.java
+++ src/main/java/handler/group/GroupDetailHandler.java
@@ -3,9 +3,13 @@ package handler.group;
 import common.command.CommandHandler;
 import dao.GroupDao;
 import dto.GroupDto;
+import dto.MemberDto;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,9 +18,12 @@ public class GroupDetailHandler implements CommandHandler {
 
     @Override
     public String process(HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+            throws ServletException, IOException, SQLException {
         if(request.getMethod().equalsIgnoreCase("GET")) {
             return processForm(request, response);
+        }
+        else if(request.getMethod().equalsIgnoreCase("POST")) {
+            return processSubmit(request, response);
         }
         else {
             response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
@@ -24,14 +31,30 @@ public class GroupDetailHandler implements CommandHandler {
         }
     }
 
+    private String processSubmit(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        HttpSession session = request.getSession();
+        Object member = session.getAttribute("member");
+        GroupDto group = (GroupDto) session.getAttribute("group");
+        groupDao.joinGroup((MemberDto) session.getAttribute("member"), group);
+
+        List<String> nicknames = groupDao.selectNicknamesByGroupName(((GroupDto) session.getAttribute("group")).getGroupName());
+        request.setAttribute("nicknames", nicknames);
+        return "/group/group-detail.jsp";
+    }
+
     private String processForm(HttpServletRequest request, HttpServletResponse response) throws SQLException {
         String url = "/group/group-detail.jsp";
+        HttpSession session = request.getSession();
+
         String groupName = request.getParameter("groupName");
         GroupDto group = groupDao.selectGroupByGroupName(groupName);
+        session.setAttribute("group", group);
         request.setAttribute("group", group);
 
         List<String> nicknames = groupDao.selectNicknamesByGroupName(groupName);
         request.setAttribute("nicknames", nicknames);
         return url;
     }
+
+
 }
